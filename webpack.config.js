56aed82842a454a0cb320765c339ff4f1be47bf1
Raphael Stein <raphistein@gmail.com>
2019-07-17 15:33:08 -0400

    fully using webpack now

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
 
 module.exports = {
-    //entry: './src/index',
-    entry: './dist/index',
+    entry: './src/index',
+    //entry: './dist/index',
     mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: [
+            '.ts', '.js'
+        ],
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, './dist')
